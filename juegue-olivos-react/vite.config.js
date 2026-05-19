32610bf860982882c2767f8de52a708af2894b6e
benjamessi <benjaminmcewan020@gmail.com>
2026-05-19 19:30:20 -0300

    Add npm start script for Vite

--- juegue-olivos-react/vite.config.js
+++ juegue-olivos-react/vite.config.js
@@ -2,5 +2,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: "127.0.0.1"
+  }
 });
